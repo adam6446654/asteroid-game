f4fcd28a256e5871b7c2351ddbf6d6d7c90a0b2b
adam6446654 <adamshahid20060000@gmail.com>
2025-07-07 11:12:19 +0500

    added ship and left/right movement

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import constants
 
 def main():
     pygame.init()
+    py_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        val = py_clock.tick(60)
+        dt = val / 1000
 
 if __name__ == "__main__":
     main()

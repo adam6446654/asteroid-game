523dc5ac049569874d0f77c622ec3bf5e67a5d14
adam6446654 <adamshahid20060000@gmail.com>
2025-07-07 11:12:39 +0500

    added ship and left/right movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 
 def main():
     pygame.init()
@@ -9,11 +10,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    player_object = player.Player(constants.SCREEN_WIDTH / 2,constants.SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player_object.update(dt)
+        player_object.draw(screen)
         pygame.display.flip()
         val = py_clock.tick(60)
         dt = val / 1000
